2aa72c2b92f03b03d72dabf86138c46bef9ec2be
Ev-Virinskaya <78601794+Ev-Virinskaya@users.noreply.github.com>
2022-06-07 13:12:15 +0400

    Deleted comments

--- src/main/java/mainTask/parking/v2/Car.java
+++ src/main/java/mainTask/parking/v2/Car.java
@@ -48,35 +48,4 @@ public class Car implements Runnable {
 
 
     }
-
-//    @Override
-//    public void run() {
-//        System.out.printf("Машина №%d подъехала к парковке.\n", carId);
-//        try {
-//            if (Parking.semaphore.tryAcquire(1, TimeUnit.SECONDS)) {
-//                int parkingNumber = -1;
-//                synchronized (Parking.parkingPlaces) {
-//                    for (int i = 0; i < Parking.parkingPlaces.length; i++) {
-//                        if (!Parking.parkingPlaces[i]) {
-//                            Parking.parkingPlaces[i] = true;
-//                            parkingNumber = i;
-//                            System.out.printf("Машина №%d припаркована на месте %d.\n", carId, i + 1);
-//                            break;
-//                        }
-//                    }
-//                }
-//                Thread.sleep((long) (Math.random() * 10000));
-//
-//                synchronized (Parking.parkingPlaces) {
-//                    Parking.parkingPlaces[parkingNumber] = false;
-//                }
-//                Parking.semaphore.release();
-//                System.out.printf("Машина №%d уехала с парковки.\n", carId);
-//            } else {
-//                System.out.printf("Машина %d уехала на другую парковку\n", carId);
-//            }
-//        }   catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
